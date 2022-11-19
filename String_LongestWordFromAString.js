4eac83696b6bd69889e66b29d5a9b869e742cdd8
Sasabh <sasabh@live.com>
2022-11-19 23:38:02 +0530

    Longest Word From A String

--- String_LongestWordFromAString.js
+++ String_LongestWordFromAString.js
@@ -1,6 +1,18 @@
 // To find longest word from a string
 
 let longString =
-    'My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.';
+    'My very photogenic mother died in a freak accident ( picnic , lightning ) when I was three , and , save for a pocket of warmth in the darkest past , nothing of her subsists within the hollows and dells of memory , over which , if you can still stand my style ( I am writing under observation ) , the sun of my infancy had set: surely , you all know those redolent remnants of day suspended , with the midges , about some hedge in bloom or suddenly entered and traversed by the rambler , at the bottom of a hill , in the summer dusk; a furry warmth , golden midges.';
 
-const findTheLogestWord = (str) => {};
+const findTheLogestWord = (str) => {
+    let strArr = str.split(' ');
+    let maxLenIndex = 0;
+    strArr.forEach((x, i) => {
+        if (x.length > strArr[maxLenIndex].length) {
+            maxLenIndex = i;
+        }
+    });
+
+    return strArr[maxLenIndex];
+};
+
+console.log('Longest word in the sentence: ', findTheLogestWord(longString));
